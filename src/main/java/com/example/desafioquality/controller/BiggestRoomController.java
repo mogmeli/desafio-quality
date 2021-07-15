ac176635c94890be51daec40bda61c27e04d77d5
LarissaGMalagoli <larissa.malagoli@mercadolivre.com>
2021-07-15 14:24:57 -0300

    feat(biggest-room): Refactoring BiggestRoom controller

--- src/main/java/com/example/desafioquality/controller/BiggestRoomController.java
+++ src/main/java/com/example/desafioquality/controller/BiggestRoomController.java
@@ -1,12 +1,13 @@
 package com.example.desafioquality.controller;
 
 import com.example.desafioquality.models.Property;
+import com.example.desafioquality.models.Room;
 import com.example.desafioquality.service.BiggestRoomService;
 import com.example.desafioquality.service.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,9 +21,10 @@ public class BiggestRoomController {
     PropertyService propertyService;
 
     @GetMapping("/{id}/biggestroom")
-    public String orderSellerFollowers(@PathVariable Long id){
+    public ResponseEntity<Room> getBiggestRoom(@PathVariable Long id){
         Property property = propertyService.getById(id);
-        return "Maior comodo:" + biggestRoomService.findBiggestRoom(property).getName();
+
+        return ResponseEntity.ok(biggestRoomService.findBiggestRoom(property));
     }
 
 }

397ac0f2c992488b1465b20262a7c91e0a9c11a4
wagner negrao <wagnernegrao@mercadolivre.com>
2021-07-15 16:26:36 -0300

    Foi criado Dto para retorno. Feito João

---
+++ src/main/java/com/example/desafioquality/controller/dto/PropertyTotalAreaDto.java
@@ -0,0 +1,19 @@
+package com.example.desafioquality.controller.dto;
+
+
+import lombok.Getter;
+
+
+@Getter
+public class PropertyTotalAreaDto {
+
+    private final String propName;
+    private final long roomsCount;
+    private final double size;
+
+    public PropertyTotalAreaDto(String propName, long roomsCount, double size) {
+        this.propName = propName;
+        this.roomsCount = roomsCount;
+        this.size = size;
+    }
+}

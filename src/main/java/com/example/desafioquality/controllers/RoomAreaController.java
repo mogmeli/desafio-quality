d01af963b26049351c558c99566d8e4f22a26a4b
pedro-cattel <pedro.cattel@mercadolivre.com>
2021-07-16 10:57:59 -0300

    features: Changing the DTOCreation to controller.

--- src/main/java/com/example/desafioquality/controllers/RoomAreaController.java
+++ src/main/java/com/example/desafioquality/controllers/RoomAreaController.java
@@ -1,23 +1,38 @@
 package com.example.desafioquality.controllers;
 
 import com.example.desafioquality.dto.PropertyAreaDto;
+import com.example.desafioquality.dto.RoomAreaDto;
+import com.example.desafioquality.models.Property;
+import com.example.desafioquality.models.Room;
+import com.example.desafioquality.service.PropertyService;
 import com.example.desafioquality.service.RoomAreaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 public class RoomAreaController {
 
+    @Autowired
     RoomAreaService roomAreaService;
 
-    public RoomAreaController(RoomAreaService roomAreaService) {
-        this.roomAreaService = roomAreaService;
-    }
+    @Autowired
+    PropertyService propertyService;
 
     @GetMapping("/roomArea/{id}")
     public ResponseEntity<PropertyAreaDto> getRoomArea(@PathVariable Long id){
-        return ResponseEntity.ok(roomAreaService.getRoomAreas(id));
+        Property property = propertyService.getById(id);
+        List<Room> rooms = property.getRooms();
+        List<RoomAreaDto> areaDtos = rooms
+                .stream()
+                .map(e -> new RoomAreaDto(e.getName(), (roomAreaService.calculaArea(e))))
+                .collect(Collectors.toList());
+        PropertyAreaDto propertyAreaDto = new PropertyAreaDto(property.getName(), areaDtos);
+        return ResponseEntity.ok(propertyAreaDto);
     }
 }
--- src/main/java/com/example/desafioquality/service/RoomAreaService.java
+++ src/main/java/com/example/desafioquality/service/RoomAreaService.java
@@ -1,40 +1,20 @@
 package com.example.desafioquality.service;
-
-import com.example.desafioquality.dto.PropertyAreaDto;
-import com.example.desafioquality.dto.RoomAreaDto;
-import com.example.desafioquality.models.Property;
 import com.example.desafioquality.models.Room;
 import com.example.desafioquality.repositories.PropertyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Service
 public class RoomAreaService {
 
+    @Autowired
     PropertyRepository propertyRepository;
 
+    @Autowired
     PropertyService propertyService;
-
-    public RoomAreaService(PropertyRepository propertyRepository, PropertyService propertyService) {
-        this.propertyRepository = propertyRepository;
-        this.propertyService = propertyService;
-    }
-
-    public List<RoomAreaDto> calculaRoomAreas(List<Room> rooms){
-        return rooms
-                .stream()
-                .map(e -> new RoomAreaDto(e.getName(), (calculaArea(e))))
-                .collect(Collectors.toList());
-    }
-
-    public PropertyAreaDto getRoomAreas(Long id){
-        Property property = propertyService.getById(id);
-        List<RoomAreaDto> areadto = calculaRoomAreas(property.getRooms());
-        return new PropertyAreaDto(property.getName(),areadto);
-    }
 
     public double calculaArea(Room room){
         return room.getWidth()*room.getLength();
